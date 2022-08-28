0c10e1e50e2e18a1d0421d5181b16dc65c837f6c
totalrickall <crimsonperry@gmail.com>
2022-08-28 17:19:52 -0400

    minor code cleanup

--- BlazorApp/Data/Controllers/RedditController.cs
+++ BlazorApp/Data/Controllers/RedditController.cs
@@ -8,36 +8,6 @@ using Newtonsoft.Json;
 
 namespace BlazorApp.Data.Controllers
 {
-    //private IEnumerable<GitHubBranch> branches = Array.Empty<GitHubBranch>();
-    //private bool getBranchesError;
-    //private bool shouldRender;
-
-    //protected override bool ShouldRender() => shouldRender;
-
-    //protected override async Task OnInitializedAsync()
-    //{
-    //    var request = new HttpRequestMessage(HttpMethod.Get,
-    //        "https://www.reddit.com/r/wallstreetbets.json");
-    //    request.Headers.Add("Accept", "application/vnd.github.v3+json");
-    //    request.Headers.Add("User-Agent", "HttpClientFactory-Sample");
-
-    //    var client = ClientFactory.CreateClient();
-
-    //    var response = await client.SendAsync(request);
-
-    //    if (response.IsSuccessStatusCode)
-    //    {
-    //        using var responseStream = await response.Content.ReadAsStreamAsync();
-    //        branches = await JsonSerializer.DeserializeAsync
-    //            <IEnumerable<GitHubBranch>>(responseStream);
-    //    }
-    //    else
-    //    {
-    //        getBranchesError = true;
-    //    }
-
-    //    shouldRender = true;
-    //}
     public class RedditController
     {
         private string baseUrl = "https://www.reddit.com/";
