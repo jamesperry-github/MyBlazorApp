2a7703cfe1f046825ebffcaef751566e5dc7e02b
jamesperry-github <crimsonperry.github@gmail.com>
2022-10-17 08:45:42 -0500

    created singleton storage for stock query, planning to store each stock query in memory

--- BlazorApp/Data/Controllers/AlphaVantageController.cs
+++ BlazorApp/Data/Controllers/AlphaVantageController.cs
@@ -21,16 +21,22 @@ namespace BlazorApp.Data.Controllers
         // /query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=full&apikey=demo
         public async Task<List<TimeSeries>> GetTimeSeries(string tickerSymbol, string duration)
         {
-            if (_tsm.GetRecordList().Count < 1 || tickerSymbol != _tsm.GetTicker())
+            if (_tsm.GetRecordList(duration).Count < 1 || tickerSymbol != _tsm.GetTicker())
             {
                 try
                 {
                     //string url = $"{baseUrl}/query?function=TIME_SERIES_DAILY&symbol={ticketSymbol}&outputsize=full&apikey={api_key}";
-                    string url = $"{baseUrl}/query?function=TIME_SERIES_INTRADAY&symbol={tickerSymbol}&interval=5min&apikey={api_key}";
+                    // TODO: GET ALL THE DATA AND PARSE DATE INTERVALS ******************************* add output size full
+                    string url = $"{baseUrl}/query?function=TIME_SERIES_INTRADAY&symbol={tickerSymbol}&interval=5min&outputsize=full&apikey={api_key}";
                     var jsondata = await new AlphaVantageService().requestJson(url);
                     //MetaData metaData = JObject.Parse(jsondata)["Meta Data"].ToObject<MetaData>();
                     dynamic timeSeries = JObject.Parse(jsondata)["Time Series (5min)"].Reverse();
-                    // TODO: move to serivce file
+                    // TODO: move to serivce file *******************************
+                    if (tickerSymbol != _tsm.GetTicker())
+                    {
+                        //_tsm.ClearTicker();
+                        _tsm.storeTicker(tickerSymbol);
+                    }
                     foreach (var item in timeSeries)
                     {
                         _tsm.CreateRecord(new TimeSeries
@@ -41,9 +47,9 @@ namespace BlazorApp.Data.Controllers
                             low = item.Value["3. low"],
                             close = item.Value["4. close"],
                             volume = item.Value["5. volume"],
-                        });
+                        }, duration, tickerSymbol);
                     }
-                    _tsm.storeTicker(tickerSymbol);
+                    _tsm.CreateStorage(duration, _tsm.GetRecordList(duration));
                     Console.WriteLine("");
                 }
                 catch (Exception)
@@ -51,7 +57,7 @@ namespace BlazorApp.Data.Controllers
                     throw;
                 }
             }
-            return await Task.FromResult(_tsm.GetRecordList());
+            return await Task.FromResult(_tsm.GetRecordList(duration));
         }
         // (*) DOWNLOAD TIME SERIES
         // /query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo&datatype=csv
--- BlazorApp/Data/Services/TimeSeriesManager.cs
+++ BlazorApp/Data/Services/TimeSeriesManager.cs
@@ -3,24 +3,77 @@
 namespace BlazorApp.Data.Services;
 public class TimeSeriesManager
 {
-    // storing in memory instead of DB for this scenario
-    // static to prevent Dictionary object from being created every request
-    private static readonly Dictionary<string, TimeSeries> _timeSeries = new();
-    private static readonly Dictionary<string, Dictionary<string, TimeSeries>> _series = new();
-    public void CreateRecord(TimeSeries item)
+    // static to prevent Dictionary object from being created every request ... store each stock
+    private static readonly Dictionary<string, string> ticker = new();
+    private static readonly Dictionary<string, TimeSeries> _dayTimeSeries = new();
+    private static readonly Dictionary<string, TimeSeries> _weekTimeSeries = new();
+    private static readonly Dictionary<string, TimeSeries> _monthTimeSeries = new();
+    private static readonly Dictionary<string, TimeSeries> _yearTimeSeries = new();
+    // main storage
+    private static readonly Dictionary<string, List<TimeSeries>> _timeSeries = new();
+
+    public string CreateRecord(TimeSeries item, string duration, string tickerSymbol)
     {
-        _timeSeries.Add(item.date.ToString(), item);
+        switch (duration)
+        {
+            case "Day":
+                _dayTimeSeries.Add($"{item.date}-{duration}-{tickerSymbol}", item);
+                return duration;
+            case "Week":
+                _weekTimeSeries.Add($"{item.date}-{duration}-{tickerSymbol}", item);
+                return duration;
+            case "Month":
+                _monthTimeSeries.Add($"{item.date}-{duration}-{tickerSymbol}", item);
+                return duration;
+            case "Year":
+                _yearTimeSeries.Add($"{item.date}-{duration}-{tickerSymbol}", item);
+                return duration;
+            default:
+                Console.WriteLine();
+                return duration;
+        }
     }
-    public List<TimeSeries> GetRecordList()
+    public void CreateStorage(string duration, List<TimeSeries> list)
     {
-        return _timeSeries.Values.ToList();
+        _timeSeries.Add(duration, list);
+    }
+    public List<TimeSeries> GetRecordList(string duration)
+    {
+        List<TimeSeries> list = new();
+        switch (duration)
+        {
+            case "Day":
+                return _dayTimeSeries.Values.ToList();
+            case "Week":
+                return _weekTimeSeries.Values.ToList();
+            case "Month":
+                return _monthTimeSeries.Values.ToList();
+            case "Year":
+                return _yearTimeSeries.Values.ToList();
+            default:
+                return list;
+        }
     }
     public void storeTicker(string symbol)
     {
-        _timeSeries.Add(symbol, new TimeSeries());
+        // clear previous ticker
+        ticker.Clear();
+        // clear previous time series data
+        _dayTimeSeries.Clear();
+        _weekTimeSeries.Clear();
+        _monthTimeSeries.Clear();
+        _yearTimeSeries.Clear();
+        _timeSeries.Clear();
+        // add ticker
+        ticker.Add(symbol, symbol);
     }
     public string GetTicker()
     {
-        return _timeSeries.Last().Key;
+        if(ticker.Values.ToList().Count > 0)
+        {
+            return ticker.First().Key;
+        }
+        // return random generic value
+        return "%%%%%%%**%%%%%%%%";
     }
 }
